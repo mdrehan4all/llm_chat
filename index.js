480bd30ff90fee57fa2a073a8ab273d6d869c127
Md Rehan <mdrehan4all@gmail.com>
2025-03-08 00:02:08 +0530

    bug fixed

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ class LLMChat {
 }
 
 module.exports = new LLMChat() // Create a singleton automatically for backwards compatability
-module.exports.LiteTTS = LLMChat // Allow users to `say = new LLMChat.LLMChat()`
+module.exports.LLMChat = LLMChat // Allow users to `say = new LLMChat.LLMChat()`
